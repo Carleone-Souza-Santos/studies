d5da7d523ecaf3988b91c2b85c2141322417d4bf
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2025-10-26 22:49:34 -0300

    updat

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Imag from "../assets/dev.png"; 
+import Imag from "../../public/assets/dev.png";
 
 export default function Home() {
   return (
