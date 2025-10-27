4e986b8867103cf3575a2eeff3925ed6053f1b42
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2025-10-26 23:08:38 -0300

    updat

--- src/data/certificadosData.ts
+++ src/data/certificadosData.ts
@@ -127,7 +127,7 @@ export const projetos = [
     instituicao: "Dev club",
     linguagem: "html, css",
     ano: 2023,
-    img: "./assets/web.png",
+    img: "/assets/web.png",
     link: "https://carleone-souza-santos.github.io/Coffee-Shop/#",
   },
 
@@ -165,7 +165,7 @@ export const projetos = [
     instituicao: "Financ",
     linguagem: "html, css ,javascript",
     ano: 2024,
-    img: "./assets/grafic.png",
+    img: "/assets/grafic.png",
     link: "https://s-gestor-ciys.vercel.app/",
   },
   {
@@ -174,7 +174,7 @@ export const projetos = [
     instituicao: "Projeto Grupo",
     linguagem: "typescript + tailwindcss + vite",
     ano: 2025,
-    img: "./assets/app.png",
+    img: "/assets/app.png",
     link: "https://procedure-pro-check-main.vercel.app/",
   },
 
@@ -184,7 +184,7 @@ export const projetos = [
     instituicao: "Projeto Grupo",
     linguagem: "typescript + tailwindcss + vite",
     ano: 2025,
-    img: "./assets/walet.png",
+    img: "/assets/walet.png",
     link: "https://appwalet.vercel.app/",
   },
   {
@@ -193,7 +193,7 @@ export const projetos = [
     instituicao: "Hora de Codar",
     linguagem: "typescript + vite + bootstrap",
     ano: 2025,
-    img: "./assets/crud.png",
+    img: "/assets/crud.png",
     link: "https://crudtypescript.vercel.app/",
   },
 ];
